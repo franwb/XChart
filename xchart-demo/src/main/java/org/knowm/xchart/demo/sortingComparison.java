be9a5699c05d9cbded1d7af83b36fd118685e3f2
franwb <frannyb1453@gmail.com>
2023-04-26 13:12:58 -0500

    Slaying????

--- xchart-demo/src/main/java/org/knowm/xchart/demo/sortingComparison.java
+++ xchart-demo/src/main/java/org/knowm/xchart/demo/sortingComparison.java
@@ -18,43 +18,86 @@ import org.knowm.xchart.BitmapEncoder.BitmapFormat;
 public class sortingComparison {
     
     public static void main(String[] args) throws IOException{
+        double[] xDataQ = new double[4];
+        double[] yDataQ = new double[4];
+
+        double[] xDataR = new double[4];
+        double[] yDataR = new double[4];
+
+        double[] xDataB = new double[4];
+        double[] yDataB = new double[4];
+
+        for(int i = 1; i < 5; i++){
+            int[] arr = generateRandomArray((int)Math.pow(10, i), 0, (int)Math.pow(10, i+1));
+            List<Integer> list=new ArrayList<>();
+                for(int j=0;j<arr.length;j++){
+                    list.add(arr[j]);
+                }
+            double quickSortTime = measureSortTime(arr, "Quick Sort", (a) -> quickSort(a, 0, a.length - 1));
+            double radixSortTime = measureSortTime(arr, "Radix Sort", (a) -> radixSort(a, 10));
+            double bucketSortTime = measureSortTime(arr, "Bucket Sort", (a) -> bucketSort(a, (Collections.max(list)-Collections.min(list))/arr.length));
+
+            xDataQ[i-1] = (int)Math.pow(10, i);
+            xDataR[i-1] = (int)Math.pow(10, i);
+            xDataB[i-1] = (int)Math.pow(10, i);
+
+            yDataQ[i-1] = quickSortTime;
+            yDataR[i-1] = radixSortTime;
+            yDataB[i-1] = bucketSortTime;
+
+        }
         // sortingComparison sortComp = new sortingComparison();
-        int[] arr = generateRandomArray(1000000, 0, 10000000);
+        // int[] arr = generateRandomArray(10, 0, 100);
+        // int[] arr2 = generateRandomArray(100, 0, 1000);
         // int x = measureSortTime(arr, "Quick Sort", quickSort(arr, arr[0], arr[(arr.length-1)]));
-        double x = 1000000;
+        // double x = 10;
+        // double x2 = 100;
         // int x2 = measureSortTime(arr, "Radix Sort", countingSort(arr, arr[0], arr[(arr.length-1)]));
         // int y2 = 10;
-        List<Integer> list=new ArrayList<>();
-        for(int i=0;i<arr.length;i++)
-        {
-            list.add(arr[i]);
-        }
+        // List<Integer> list=new ArrayList<>();
+        // for(int i=0;i<arr.length;i++)
+        // {
+        //     list.add(arr[i]);
+        // }
+        // List<Integer> list2=new ArrayList<>();
+        // for(int i=0;i<arr2.length;i++)
+        // {
+        //     list2.add(arr2[i]);
+        // }
         // int x3 = measureSortTime(arr, "Radix Sort", bucketSort(arr, (Collections.max(list)-Collections.min(list))/arr.length));
         // int y3 = 10;
 
 
-        double quickSortTime = measureSortTime(arr, "Quick Sort", (a) -> quickSort(a, 0, a.length - 1));
-        double radixSortTime = measureSortTime(arr, "Radix Sort", (a) -> radixSort(a, 10));
-        double bucketSortTime = measureSortTime(arr, "Bucket Sort", (a) -> bucketSort(a, (Collections.max(list)-Collections.min(list))/arr.length));
+        // double quickSortTime = measureSortTime(arr, "Quick Sort", (a) -> quickSort(a, 0, a.length - 1));
+        // double radixSortTime = measureSortTime(arr, "Radix Sort", (a) -> radixSort(a, 10));
+        // double bucketSortTime = measureSortTime(arr, "Bucket Sort", (a) -> bucketSort(a, (Collections.max(list)-Collections.min(list))/arr.length));
+
+        // double quickSortTime2 = measureSortTime(arr2, "Quick Sort", (a) -> quickSort(a, 0, a.length - 1));
+        // double radixSortTime2 = measureSortTime(arr2, "Radix Sort", (a) -> radixSort(a, 10));
+        // double bucketSortTime2 = measureSortTime(arr2, "Bucket Sort", (a) -> bucketSort(a, (Collections.max(list)-Collections.min(list))/arr.length));
 
 
 
-        double[] xData = new double[] {x};
-        double[] yData = new double[] {quickSortTime};
 
-        double[] xData2 = new double[] {x};
-        double[] yData2 = new double[] {radixSortTime};
+        // double[] xDataQ = new double[] {x, x2};
+        // double[] yDataQ = new double[] {quickSortTime, quickSortTime2};
 
-        double[] xData3 = new double[] {x};
-        double[] yData3 = new double[] {bucketSortTime};
+        // double[] xDataR = new double[] {x, x2};
+        // double[] yDataR = new double[] {radixSortTime, radixSortTime2};
+
+        // double[] xDataB = new double[] {x, x2};
+        // double[] yDataB = new double[] {bucketSortTime, bucketSortTime2};
+
 
         // double[] xData2 = new double[] { 0.0, 1.0, 2.0 };
         // double[] yData2 = new double[] { 1.0, 2.0, 0.0 };
 
-        XYChart chart = QuickChart.getChart("Sample Chart", "X", "Y", "y(x)", xData, yData);
+        XYChart chart = QuickChart.getChart("Sample Chart", "X", "Y", "Quick Sort", xDataQ, yDataQ);
         // add other line to graph
-        chart.addSeries("Series 2", xData2, yData2);
-        chart.addSeries("Series 3", xData3, yData3);
+
+        // chart.addSeries("Quick Sort", xDataQ, yDataQ);
+        chart.addSeries("Radix Sort", xDataR, yDataR);
+        chart.addSeries("Bucket Sort", xDataB, yDataB);
 
 
         // Show it
